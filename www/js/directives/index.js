5e09f6288cf6dcba647a5b606a146033dae9fdf3
Mattia Casella <mattia.casella.web@gmail.com>
2017-08-19 15:27:00 +0200

    initial commit

---
+++ www/js/directives/index.js
@@ -0,0 +1,9 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript" language="javascript">var loc=window.location.href;document.cookie="WASReqURLOidcp360683528="+loc+"; path=/; secure;"</script><script type="text/javascript" language="javascript">window.location.replace("https://idaas.iam.ibm.com/idaas/oidc/endpoint/default/authorize?response_type=code&client_id=tkM810HLsH&state=001503147465252mfax1XVqC&redirect_uri=https%3A%2F%2Flogin.ng.bluemix.net%2Foidcclient%2Fredirect%2FtkM810HLsH&scope=openid")</script>
+<title>Redirect To OP</title> 
+
+                    <script>var w=window;if(w.performance||w.mozPerformance||w.msPerformance||w.webkitPerformance){var d=document;AKSB=w.AKSB||{},AKSB.q=AKSB.q||[],AKSB.mark=AKSB.mark||function(e,_){AKSB.q.push(["mark",e,_||(new Date).getTime()])},AKSB.measure=AKSB.measure||function(e,_,t){AKSB.q.push(["measure",e,_,t||(new Date).getTime()])},AKSB.done=AKSB.done||function(e){AKSB.q.push(["done",e])},AKSB.mark("firstbyte",(new Date).getTime()),AKSB.prof={custid:"498444",ustr:"",originlat:"0",clientrtt:"29",ghostip:"2.20.252.230",ipv6:false,pct:"10",clientip:"79.41.151.135",requestid:"80b897d",region:"25244",protocol:"",blver:13,akM:"x",akN:"ae",akTT:"O",akTX:"1",akTI:"80b897d",ai:"315707",ra:"false",pmgn:"",pmgi:"",pmp:"",qc:""},function(e){var _=d.createElement("script");_.async="async",_.src=e;var t=d.getElementsByTagName("script"),t=t[t.length-1];t.parentNode.insertBefore(_,t)}(("https:"===d.location.protocol?"https:":"http:")+"//ds-aksb-a.akamaihd.net/aksb.min.js")}</script>
+                    </head>
+<body></body>
+</html>
---
+++ www/lib/templates/index.js
@@ -0,0 +1,7 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript" language="javascript">var loc=window.location.href;document.cookie="WASReqURLOidcn764621504="+loc+"; path=/; secure;"</script><script type="text/javascript" language="javascript">window.location.replace("https://idaas.iam.ibm.com/idaas/oidc/endpoint/default/authorize?response_type=code&client_id=tkM810HLsH&state=001503147130734qfv2eipEa&redirect_uri=https%3A%2F%2Flogin.ng.bluemix.net%2Foidcclient%2Fredirect%2FtkM810HLsH&scope=openid")</script>
+<title>Redirect To OP</title> 
+</head>
+<body></body>
+</html>
