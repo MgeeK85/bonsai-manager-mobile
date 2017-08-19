39a4dbf69076ae9b5a1542065796f1c731462c2b
Mattia Casella <mattia.casella.web@gmail.com>
2017-08-19 18:59:53 +0200

    fix templates and directives

--- www/js/templates/index.js
+++
@@ -1,7 +0,0 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script type="text/javascript" language="javascript">var loc=window.location.href;document.cookie="WASReqURLOidcp1366989086="+loc+"; path=/; secure;"</script><script type="text/javascript" language="javascript">window.location.replace("https://idaas.iam.ibm.com/idaas/oidc/endpoint/default/authorize?response_type=code&client_id=tkM810HLsH&state=001503147465245R6umZLNOZ&redirect_uri=https%3A%2F%2Flogin.ng.bluemix.net%2Foidcclient%2Fredirect%2FtkM810HLsH&scope=openid")</script>
-<title>Redirect To OP</title> 
-</head>
-<body></body>
-</html>
